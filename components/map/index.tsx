a5cf2f2d923d65dd1fecd29d459426727e1d1f9a
NotLucca <jeanluccadias@gmail.com>
2022-09-09 14:29:55 -0300

    adding geolocation

--- components/map/index.tsx
+++ components/map/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useCallback, useRef, Key } from "react";
+import { useState, useMemo, useCallback, useRef, Key, useEffect } from "react";
 import Places from "../places/index";
 import {
   GoogleMap,
@@ -13,16 +13,33 @@ import {
 import stopData from "./csvjson.json";
 
 
-
 type LatLngLiteral = google.maps.LatLngLiteral;
 type DirectionsResult = google.maps.DirectionsResult;
 type MapOptions = google.maps.MapOptions;
 type MapArray = any;
 
+const center = { lat: 43.45, lng: 23.49 }
+
+
 export default function Map() {
   const [office, setOffice] = useState<LatLngLiteral>();
+  const [userLocation, setUserLocation] = useState({ lat: 43.45, lng: 23.49 });
+  const getGeoLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        setUserLocation({
+          lat: latitude,
+          lng: longitude,
+        });
+      });
+    }
+  };
+
+  useEffect(() => {getGeoLocation()}, [])
+  
   const mapRef = useRef<GoogleMap>();
-  const center = useMemo<LatLngLiteral>(() => ({ lat: 43.45, lng: 23.49 }), []);
+  // const center = useMemo<LatLngLiteral>(() => ({ lat: 43.45, lng: 23.49 }), []);
   const options = useMemo<MapOptions>(
     () => ({
       disableDefaultUI: true,
@@ -32,6 +49,8 @@ export default function Map() {
   );
   const onLoad = useCallback((map: any) => (mapRef.current = map), []);
   const data = require("./csvjson.json")
+  
+  
     
   return (
     <div className="container">
@@ -57,12 +76,11 @@ export default function Map() {
       <div className="map">
         <GoogleMap
           zoom={13}
-          center={center}
+          center={userLocation ? userLocation : center}
           mapContainerClassName="map-container"
           options={options}
           onLoad={onLoad}
         >
-         
           <MarkerClusterer>
             {(clusterer) => data.map((stop: { stop_lat: any; stop_lon: any; stop_name: any; }, index: Key | null | undefined) => <Marker clusterer={clusterer} position={{lat: stop.stop_lat, lng: stop.stop_lon}} key={index}></Marker>) }
           </MarkerClusterer>
